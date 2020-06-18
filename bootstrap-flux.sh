495744fb7037ff6d4f605aec7b1884f97c86a615
CJ Harries <cj@wizardsoftheweb.pro>
2020-06-18 01:59:25 -0500

    Tidy script

--- bootstrap-flux.sh
+++ bootstrap-flux.sh
@@ -8,12 +8,18 @@ helm repo add fluxcd https://charts.fluxcd.io
 kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/crds.yaml
 kubectl create namespace flux
 helm upgrade -i flux fluxcd/flux \
-   --set git.url=git@github.com:thecjharries/k3s-vagrant-cluster \
-   --namespace flux
+    --set git.url=git@github.com:thecjharries/k3s-vagrant-cluster-flux \
+    --set git.branch=main \
+    --namespace flux
 
- helm upgrade -i helm-operator fluxcd/helm-operator \
-   --set git.ssh.secretName=flux-git-deploy \
-   --namespace flux
+kubectl wait --for=condition=ready pod -l app=flux
+
+helm upgrade -i helm-operator fluxcd/helm-operator \
+    --set helm.versions=v3 \
+    --set git.ssh.secretName=flux-git-deploy \
+    --namespace flux
+
+kubectl wait --for=condition=ready pod -l app=helm-operator
 
 KEY_CONTENTS=$(kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2)
 
